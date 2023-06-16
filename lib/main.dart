92855f46ee1a022690bfd0222994b6af2344b844
Euphoria99 <363pavanbhat@gmail.com>
2023-06-17 01:21:32 +0530

    Hiding SliverAppbar title with animated opacity

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.orange,
         useMaterial3: true,
       ),
-      home: const SafeArea(
+      home: SafeArea(
         child: CustomSliver(),
       ),
     );
