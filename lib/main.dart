0565dcb5398a59ce1dce20f5c41844a05d59c25e
Euphoria99 <363pavanbhat@gmail.com>
2023-06-21 22:11:39 +0530

    Scrolling Floating Action button and get scroll positions

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_widgets/screens/gridview_screen.dart';
 import 'package:flutter_widgets/screens/home_screen.dart';
 import 'package:flutter_widgets/widgets/bottom_sheet.dart';
 
+import 'screens/fab_scroll_position.dart';
 import 'screens/sliding_panel_screen.dart';
 import 'screens/sliver_app_bar.dart';
 
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: SafeArea(
-        child: SlidingPanelScreen(),
+        child: SevasScreenOld(),
       ),
     );
   }
